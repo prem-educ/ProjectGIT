8e5ca99e124436aac434076ac6f3ad440bd681f2
unknown <jelilahounou@gmail.com>
2022-09-29 12:42:20 +0200

    Modification du script pour qu'il affiche les nombres premiers de 1 à 1000000

--- scriptnumbers.py
+++ scriptnumbers.py
@@ -1,4 +1,4 @@
-for nombre in range (1, 100000): #Pour la variable nombre comprus entre 1 et 100000
+for nombre in range (1, 1000000): #Pour la variable nombre comprus entre 1 et 100000
     count = 0 #On initialise la var count à zero
     for i in range(2, (nombre//2 + 1)):
         if(nombre % i == 0): #Si le reste de la division de nombre et i et 0
