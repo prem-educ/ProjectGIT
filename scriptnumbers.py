0c76c5a0c35b2cce717e293456e3e07ebef02d54
unknown <jelilahounou@gmail.com>
2022-09-29 12:14:43 +0200

    Commit du scripy des nombres premiers avec les commentaires

--- scriptnumbers.py
+++ scriptnumbers.py
@@ -1,9 +1,9 @@
-for Number in range (1, 100000):
-    count = 0
-    for i in range(2, (Number//2 + 1)):
-        if(Number % i == 0):
-            count = count + 1
+for nombre in range (1, 100000): #Pour la variable nombre comprus entre 1 et 100000
+    count = 0 #On initialise la var count à zero
+    for i in range(2, (nombre//2 + 1)):
+        if(nombre % i == 0): #Si le reste de la division de nombre et i et 0
+            count = count + 1 #la variable count est maintenant égale à sa valeur initiale + 1
             break
 
-    if (count == 0 and Number != 1):
-        print(" %d" %Number, end = '  ')
+    if (count == 0 and nombre != 1):#Si la valeur de count est 0 et la valeur de nombre est different de 1 
+        print(" %d" %nombre, end = '  ')#Affiche les nombres premiers
